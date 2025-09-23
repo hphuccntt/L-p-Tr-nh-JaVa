712020907fd3428819e01cd1ad0a6926f9dbd8a2
hphuccntt <phuc54212420014@bachkhoasaigon.edu.vn>
2025-09-23 11:28:25 +0700

    Thêm Bài 20

---
+++ src/spc/edu/Bai20_ArrayList_01.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package spc.edu;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Bai20_ArrayList_01 {
+    public static void main(String[] args) {
+        ArrayList<String> s = new ArrayList<String>();
+        s.add("Hoàng");
+        s.add("Khoa");
+        s.add("Kiệt");
+        s.add("Huy");
+        for(i = 1; i)
+    }
+}
